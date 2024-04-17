575e2dea31a8d831b9df24661c5cca91ff1d0ac3
reyjager <86574904+reyjager@users.noreply.github.com>
2024-04-17 16:32:05 +0300

    create main.dart

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Reyjager Coffee'),
+            backgroundColor: Colors.brown[700],
+            centerTitle: true,
+          ),
+          body: const Text('Zip your Coffee Now'))));
+}
